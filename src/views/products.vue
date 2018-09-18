7c34cb4a4c1fe5c86bc776cf00e4b8f219219ffd
Chad Ridings <chadridings@gmail.com>
2018-09-17 19:32:20 -0700

    [master] functionality added to keep decimal's. select options created via loop.

--- src/views/products.vue
+++ src/views/products.vue
@@ -23,26 +23,7 @@
                 <div class="price">${{ product.price }}</div>
                 <div class="select-box">
                     <select v-model="product.quantity">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
+                        <option v-for="n in 20" v-bind:key="n">{{ n }}</option>
                     </select>
                 </div>
             </div>
@@ -76,35 +57,35 @@
 </style>
 
 <script>
+    function keepDecimal(num) {
+        return Number.parseFloat(num).toFixed(2);
+    }
+
     export default {
         name: 'products',
         data: () => ({
-            products: [
-                {
+            products: [{
                     "id": 1,
                     "name": "Sunflower Melody",
-                    "price": 30.00,
+                    "price": keepDecimal(30.00),
                     "quantity": 1,
                     "image": "assets/images/flower-1.png"
-                },
-                {
+                }, {
                     "id": 2,
                     "name": "Spring Mix",
-                    "price": 35.00,
+                    "price": keepDecimal(35.00),
                     "quantity": 1,
                     "image": "assets/images/flower-2.png"
-                },
-                {
+                }, {
                     "id": 3,
                     "name": "Colorful Arrangement",
-                    "price": 40.00,
+                    "price": keepDecimal(40.00),
                     "quantity": 1,
                     "image": "assets/images/flower-3.png"
-                },
-                {
+                }, {
                     "id": 4,
                     "name": "Tulip Season",
-                    "price": 50.00,
+                    "price": keepDecimal(50.00),
                     "quantity": 1,
                     "image": "assets/images/flower-4.png"
                 }
