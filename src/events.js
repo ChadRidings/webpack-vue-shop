d5ce3579fa83a06e0caf9e7e2522d4046031cadc
Chad Ridings <chadridings@gmail.com>
2018-09-23 16:11:13 -0700

    [master] example on how to use the Event Bus added

--- src/events.js
+++ src/events.js
@@ -2,3 +2,23 @@ import Vue from 'vue';
 
 const EventBus = new Vue();
 export default EventBus;
+
+/*
+    Event Bus Example Use:
+
+
+    In the file you wish to emit the event from...
+
+    import EventBus from '../events';
+    EventBus.$emit('emitEvent', data.product.id);
+
+    
+
+    In the file you wish to capture the event from...  
+
+    import EventBus from '../events';
+    EventBus.$on('emitEvent', data => {
+        *** Do Stuff Here ***
+    });
+
+*/
